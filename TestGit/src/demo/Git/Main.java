c1ccbf65ab2208ca11eb647ff6c586115060f458
kucute <kid2357@gmail.com>
2016-09-28 14:46:13 +0700

    đây là bài tập ví dụ nè

---
+++ TestGit/src/demo/Git/Main.java
@@ -0,0 +1,17 @@
+package demo.Git;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data= new SalesData();
+		displaygreeting();
+		data.display();
+		// TODO Auto-generated method stub
+
+	}
+	private static void displaygreeting(){
+		System.out.println("Hello World");
+		System.out.println("Xin Chao");
+	}
+
+}
---
+++ TestGit/src/demo/Git/SalesData.java
@@ -0,0 +1,9 @@
+package demo.Git;
+
+public class SalesData {
+	int data[] = {0,4,42};
+	public void display(){
+		System.out.println("TODO: Implement data display");
+	}
+
+}
